a15fca9b0fe30ebd743b5ac231eeb6100c7ec1d4
ANTE BOZIC <antebozic@gmail.com>
2017-12-07 10:11:17 -0500

    partial solution

---
+++ controllers/messages.js
@@ -0,0 +1,17 @@
+
+const express       = require('express')
+const Candidate     = require('../db/connection.js')
+
+const router        = express.Router()
+
+
+router.get('/', (req, res) => {
+  Message.find({}).then((messages) =>{
+    res.render('messages-index', {
+      messages: messages
+    })
+  })
+})
+
+
+module.exports = router
