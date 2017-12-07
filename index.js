0b4e9f28f65f448cc75675b0eb344b2b5d5a98fa
Meg MacIver <megmaciver13@gmail.com>
2017-12-07 09:17:21 -0500

    install express, hbs, and body-parser; set up index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const express = require('express')
+const hbs = require('hbs')
+const parser = require('body-parser')
+
+const app = express()
+
+app.set('view engine', 'hbs')
+
+app.use(parser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+
+
+app.listen(3000, () => {
+  console.log('You are now connected to port 3000!')
+})
+
 module.exports = app
