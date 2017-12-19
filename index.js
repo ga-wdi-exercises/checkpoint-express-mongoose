406befa1b76e37c9860682aba3aa0fcf75024990
ANTE BOZIC <antebozic@gmail.com>
2017-12-19 14:00:06 -0500

    final version

---
+++ index.js
@@ -0,0 +1,27 @@
+const mongoose = require('./db/connection')
+const express = require('express')
+const hbs = require('hbs')
+const parser = require('body-parser')
+
+const messages = require('./controllers/messages')
+
+const Message = mongoose.model('Message')
+
+const app = express()
+
+app.set('view engine', 'hbs')
+
+app.use(parser.json())
+app.use(parser.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+	res.redirect('/messages')
+})
+
+app.use('/messages', messages)
+
+app.listen(3000, () => {
+	console.log('you are connected')
+})
+
+module.exports = app
